0435610e4b47adb2b4e24707cde039ffb1ede40e
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-05-19 05:06:24 -0400

    added code to make test pass

--- src/isbntools/ValidateIsbn.java
+++ src/isbntools/ValidateIsbn.java
@@ -3,6 +3,9 @@ package isbntools;
 public class ValidateIsbn {
 
     public boolean checkISBN(String isbn) {
+        if(isbn.length() != 10) {
+            throw new NumberFormatException("ISBN must be 10 digits long");
+        }
         int total = 0;
         for (int i = 0; i < 10; i++) {
             total += isbn.charAt(i) * (10 - i);
